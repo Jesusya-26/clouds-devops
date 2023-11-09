5f780a017c0ad927c2dc61113b0b463835bf6000
Jesusya-26 <rus.babaef@yandex.ru>
2023-11-10 00:17:18 +0300

    add readme and docker files

---
+++ lab-2/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 500))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
